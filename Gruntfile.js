ffb6d0971e67c17d2a5cbd56d81e73d94066e736
Sven <sven@mutzl.org>
2017-10-29 09:36:36 +0000

    Grunt automated testing and minimization

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,44 @@
 module.exports = function(grunt) {
-	  grunt.initConfig({
-		      pkg: grunt.file.readJSON('package.json'),
-		      browserify: {
-			            './dist/bundle.js': ['./index.js']
-			          }
-		    })
-	  grunt.loadNpmTasks('grunt-browserify')
-}
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        mochaTest: {
+            test: {
+                options: {
+                    reporter: 'spec',
+                    captureFile: './dist/result.txt',
+                    quiet: false,
+                    clearRequireCache: false, 
+                    noFail: false
+                },
+                src: ['./test/*.js']
+            }
+        },
+        browserify: {
+            './dist/metaverse.js': ['./index.js']
+        },
+        babel: {
+            options: {
+                sourceMap: true,
+                presets: ['env']
+            },
+            dist: {
+                files: {
+                    './dist/metaverse.js': './dist/metaverse.js'
+                }
+            }
+        },
+        uglify: {
+            js: {
+                src: ['./dist/metaverse.js'],
+                dest: './dist/metaverse.min.js'
+            }
+        }
+
+    });
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-babel');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-mocha-test');
+    
+    grunt.registerTask('default', ['mochaTest', 'browserify', 'babel', 'uglify']);
+};
