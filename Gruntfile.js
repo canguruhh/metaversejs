cb49ba108426079a4033b176073d777ac39baedb
Sven <sven@mutzl.org>
2017-10-30 15:39:10 +0000

    Browserify fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,7 +21,7 @@ module.exports = function(grunt) {
                         standalone: 'Metaverse'
                     }
                 },
-                src: ['./src/index.js'],
+                src: ['./index.js'],
                 dest: './dist/metaverse.js',
             }
         },
