d1e4c0bc17e247f10ea9894533e8c25b30fc2b9b
Laurent Salou <laurent.salou@hotmail.fr>
2020-03-06 12:56:44 +0100

    Update testnet network params

--- src/networks.js
+++ src/networks.js
@@ -15,10 +15,10 @@ module.exports = {
     },
     testnet: {
         messagePrefix: '\x18Bitcoin Signed Message:\n',
-        bech32: 'tb',
+        bech32: 'bc',
         bip32: {
-            public: 0x043587cf,
-            private: 0x04358394
+            public: 0x0488b21e,
+            private: 0x0488ade4
         },
         pubKeyHash: 0x7f,
         scriptHash: 0xc4,
